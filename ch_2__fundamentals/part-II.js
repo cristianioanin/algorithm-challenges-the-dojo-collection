6a72f3a194e16c70b6691d59b99a3daf61767566
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-11-01 00:12:41 +0200

    Keeping track of days

--- ch_2__fundamentals/part-II.js
+++ ch_2__fundamentals/part-II.js
@@ -121,3 +121,128 @@ console.log(isPrime(5));
 console.log(isPrime(11));
 console.log(isPrime(13));
 console.log(isPrime(16));
+
+//  Date , on a Deserted Island
+// After a particularly fabulous New Year’s Eve party to end 2016, Eduardo wakes to find himself stranded on a deserted island. He misses his home in Burbank, but at least now he can spend plenty of time outdoors – and you can’t beat the commute! To pass the time until he is rescued, he counts sunrises. 
+// 1) Help Eduardo track what day of the week it is. Create a weekdayName(weekdayNum) function that, given a number between 1 and 7, will console.log a string containing the day of the week for that number ( given 1 , log "Sunday" ). Use a SWITCH statement. 
+// 2) Expand weekdayName () to create weekdayName2(dayNum) accepting numbers up to 365. Return weekday as before, given number of days total. "Sunday" still corresponds to 1 . 
+// 3) Create a new function someDays() that calls weekDayName2 () seventeen times, with randomly generated integers as high as 365. Log each result string. If it is a weekday, add the phrase "Work hard!" , and if it is a weekend day, add "Enjoy your day off!" 
+// 4) Build function monthName(monthNum) that, given a number from 1 to 12, returns a string containing month for that number ( "May" corresponds to 5 ). Use an array, without loops. 
+// 5) Now expand monthName () to create monthToDays(monthNum) , returning the number of days in that month, in the year 2017. Hint: use a SWITCH statement for the days in each month. 
+// 6) Despite using his ember expertise to create a glowing SOS visible from space, the days go by and sadly Eduardo is still not rescued. Is it spring yet? It might as well be. Build on monthName () to create dayToMonth(dayNum) . If given a day number since the year began, return the current month (assume it is not a Leap Year). Given 75 , return "March" . 
+// 7) Eduardo builds a Dojo bootcamp on the island. Initially his students only find Ninja Gold in caves, but eventually even his tree sloths can write code quickly! Dojo classes meet Monday t hru Friday, so let’s reincorporate weekday to our calculations. Construct fullDate(dayNum) to accept number of days so far in 2017, and return a full date string. He hardly remembers that fateful New Year’s Eve party, but he knows it was a Saturday. Given 142 , return "Monday, May 22, 2017" . 
+// 8) Times flies when you’re at a Dojo – months in fact. Build fullDate2(dayNum) that will be given a 4-digit integer: the days that have passed since December 31, 2016. This number can stretch into future years! You can assume that any year number divisible by four is a leap year and has a 29-day February. Given 8475 , return "Thursday, March 15, 2040" . 
+// 9) Eduardo hacks the Google Maps API and adds this long-forgotten island back onto the map. Soon he is rescued! News of his Hemingway-like stoicism make him famous for centuries. Build fullDate3 (dayNum) to handle days up to 140,000! Note: years 2100, 2200, and 2300 are not leap years (although 2400 is ). Given 139947 , return "Tuesday, February 29, 2400" . Having completed this epic saga, you are ready for additional concepts and chapters! Journey safely ….
+
+
+// 1) Help Eduardo track what day of the week it is. Create a weekdayName(weekdayNum) function that, given a number between 1 and 7, will console.log a string containing the day of the week for that number ( given 1 , log "Sunday" ). Use a SWITCH statement. 
+
+function weekdayName(weekdayNum) {
+  switch (weekdayNum) {
+    case 1:
+      console.log("Sunday");
+      break;
+    case 2:
+      console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thursday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Not a day of the week");
+  }
+}
+
+weekdayName(4);
+
+
+// 2) Expand weekdayName () to create weekdayName2(dayNum) accepting numbers up to 365. Return weekday as before, given number of days total. "Sunday" still corresponds to 1 . 
+
+function weekdayName2(weekdayNum) {
+  let weekday = weekdayNum % 7;
+
+  if (weekday === 0) weekday = 7;
+  weekdayName(weekday);
+
+  return weekday;
+}
+
+weekdayName2(365);
+
+
+// 3) Create a new function someDays() that calls weekDayName2 () seventeen times, with randomly generated integers as high as 365. Log each result string. If it is a weekday, add the phrase "Work hard!" , and if it is a weekend day, add "Enjoy your day off!" 
+
+function someDays() {
+  function getRandomDayNumber() {
+    return Math.floor(Math.random() * 365 + 1);
+  }
+
+  let calls = 17;
+
+  while (calls > 0) {
+    const weekdayNum = getRandomDayNumber();
+    const weekday = weekdayName2(weekdayNum);
+
+    if (weekday >= 2 && weekday <= 6) {
+      console.log('Work hard!');
+    } else {
+      console.log('Enjoy your day off!');
+    }
+
+    calls--;
+  }
+}
+
+someDays();
+
+
+// 4) Build function monthName(monthNum) that, given a number from 1 to 12, returns a string containing month for that number ( "May" corresponds to 5 ). Use an array, without loops. 
+
+function monthName(monthNum) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  if (monthNum < 1 || monthNum > 12) { return -1; }
+
+  console.log(months[monthNum - 1]);
+}
+
+monthName(5);
+
+// 5) Now expand monthName () to create monthToDays(monthNum) , returning the number of days in that month, in the year 2017. Hint: use a SWITCH statement for the days in each month. 
+
+function monthToDays(monthNum) {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (monthNum < 1 || monthNum > 12) { return -1; }
+
+  console.log(days[monthNum - 1]);
+}
+
+monthToDays(5);
+
+// 6) Despite using his ember expertise to create a glowing SOS visible from space, the days go by and sadly Eduardo is still not rescued. Is it spring yet? It might as well be. Build on monthName () to create dayToMonth(dayNum) . If given a day number since the year began, return the current month (assume it is not a Leap Year). Given 75 , return "March" . 
+
+function dayToMonth(dayNum) {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let daysOfMonthsBefore = 31;
+
+  for (let i = 1; i < days.length; i++) {
+    if (dayNum <= daysOfMonthsBefore) return monthName(i);
+    daysOfMonthsBefore += days[i];
+  }
+}
+
+dayToMonth(91);
